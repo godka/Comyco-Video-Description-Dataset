f40c49f1a53a7749d43a3a86b9ced5db9ce27472
Tianchi Huang <mythkast@hotmail.com>
2024-10-10 12:06:53 +0900

    Update dashify.py

--- tools/dashify.py
+++ tools/dashify.py
@@ -1,6 +1,8 @@
 import os
+from natsort import natsorted
+
 for video in os.listdir('./videos/'):
-    for seg in os.listdir('./videos/' + video + '/'):
+    for seg in natsorted(os.listdir('./videos/' + video + '/')):
         video_filename = './videos/' + video + '/' + seg + '/'
         os.makedirs(video_filename + 'DASH/')
         cmd = 'MP4Box -dash 4000 -segment-name %s\\ -rap -out ' + video_filename + 'DASH/' + seg + '.mpd'
